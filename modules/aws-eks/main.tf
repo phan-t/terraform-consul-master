1caefdb40a1425fc1689affe108749195035aed5
Tony Phan <tphan@hashicorp.com>
2021-07-13 22:23:06 +1000

    feat: Add variables for aws-eks module

--- modules/aws-eks/main.tf
+++ modules/aws-eks/main.tf
@@ -1,23 +1,12 @@
-data "aws_eks_cluster" "cluster" {
-  name = module.eks.cluster_id
-}
-
-data "aws_eks_cluster_auth" "cluster" {
-  name = module.eks.cluster_id
-}
-
-provider "kubernetes" {
-  host                   = data.aws_eks_cluster.cluster.endpoint
-  token                  = data.aws_eks_cluster_auth.cluster.token
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
-}
-
 module "eks" {
-  source          = "terraform-aws-modules/eks/aws"
-  cluster_name    = var.eks_cluster_name
-  cluster_version = "1.20"
-  subnets         = var.private_subnets
-  vpc_id          = var.vpc_id
+  source                          = "terraform-aws-modules/eks/aws"
+  version                         = "17.1.0"
+
+  cluster_name                    = var.cluster_name
+  cluster_version                 = var.cluster_version
+  vpc_id                          = var.vpc_id
+  subnets                         = var.private_subnets
+  cluster_endpoint_private_access = true
 
   workers_group_defaults = {
     root_volume_type = "gp2"
@@ -26,9 +15,10 @@ module "eks" {
   worker_groups = [
     {
       name                          = "worker-group-1"
-      instance_type                 = "t2.small"
-      asg_desired_capacity          = 3
-      additional_security_group_ids = var.eks_workers_mgmt_security_group_id
+      instance_type                 = var.worker_instance_type
+      key_name                      = var.key_pair_key_name
+      asg_desired_capacity          = var.asg_desired_capacity
+      additional_security_group_ids = var.workers_mgmt_security_group_id
     }
   ]
 }
