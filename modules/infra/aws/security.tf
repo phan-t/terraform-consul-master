a0d48ace77f0d3a39c46f229bac842bf2770d4e8
Tony Phan <tphan@hashicorp.com>
2021-07-15 21:49:14 +1000

    feat: Add bastion and cts nodes

--- modules/infra/aws/security.tf
+++ modules/infra/aws/security.tf
@@ -1,12 +1,49 @@
 resource "aws_security_group" "workers-mgmt" {
-  name_prefix = "${var.deployment_name}-workers-mgmt"
+  name_prefix = "${var.deployment_name}-workers-mgmt-"
   vpc_id      = module.vpc.vpc_id
 
   ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
+    cidr_blocks = module.vpc.private_subnets_cidr_blocks
+  }
+}
 
-    cidr_blocks = tolist([var.vpc_cidr])
+resource "aws_security_group" "bastion" {
+  name_prefix = "${var.deployment_name}-bastion-"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = module.vpc.private_subnets_cidr_blocks
+  }
+}
+
+resource "aws_security_group" "cts" {
+  name_prefix = "${var.deployment_name}-cts-"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress {
+    from_port       = 22
+    to_port         = 22
+    protocol        = "tcp"
+    security_groups = [aws_security_group.bastion.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    security_groups = [aws_security_group.workers-mgmt.id]
   }
 }
