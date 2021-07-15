b57915ee4fa73af42bc600f7c4365fbc4527daa8
Tony Phan <tphan@hashicorp.com>
2021-07-15 21:44:56 +1000

    feat: Add variables for eks-helm module

--- modules/consul/eks-helm/variables.tf
+++ modules/consul/eks-helm/variables.tf
@@ -18,3 +18,23 @@ variable "workers_asg_arns_id" {
   description = "Id of the autoscaling groups containing workers"
   type        = list
 }
+
+variable "private_nat_gateway_route_ids" {
+  description = "Ids of the private route table association"
+  type        = list
+}
+
+variable "public_internet_gateway_route_id" {
+  description = "Ids of the public route table association"
+  type        = list
+}
+
+variable "private_route_table_association_ids" {
+  description = "Ids of the private route table association"
+  type        = list
+}
+
+variable "public_route_table_association_ids" {
+  description = "Ids of the public route table association"
+  type        = list
+}
