c415485d667d29260bf8924e11c4df097b42d540
Tony Phan <tphan@hashicorp.com>
2021-07-15 21:46:39 +1000

    feat: Add bastion and cts nodes

--- modules/infra/aws/main.tf
+++ modules/infra/aws/main.tf
@@ -1,9 +1,45 @@
-/*provider "aws" {
-  region = var.region
-  default_tags {
-    tags = {
-      owner = var.owner
-      TTL = var.ttl
-    }
+data "aws_ami" "ubuntu20" {
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
   }
-}*/
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+  most_recent = true
+  owners = ["099720109477"]
+}
+
+resource "aws_instance" "bastion-node" {
+  ami             = data.aws_ami.ubuntu20.id
+  instance_type   = "t2.micro"
+  key_name        = var.key_pair_key_name
+  subnet_id       = element(module.vpc.public_subnets, 1)
+  security_groups = [aws_security_group.bastion.id]
+
+  tags = {
+    owner = var.owner
+    TTL = var.ttl
+  }
+}
+
+resource "aws_instance" "cts-node" {
+  ami             = "ami-0b12537d4e6b1893c"
+  instance_type   = "t2.small"
+  key_name        = var.key_pair_key_name
+  subnet_id       = element(module.vpc.private_subnets, 1)
+  security_groups = [aws_security_group.cts.id]
+
+  tags = {
+    owner = var.owner
+    TTL = var.ttl
+  }
+}
