a98079691db000c0010aee9cea0279a72e116308
Tony Phan <tphan@hashicorp.com>
2021-07-21 21:13:13 +1000

    feat: Add consul server fqdn and serf lan port

--- main.tf
+++ main.tf
@@ -20,16 +20,18 @@ resource "random_string" "suffix" {
 module "aws" {
   source  = "./modules/infra/aws"
   
-  region            = var.aws_region
-  owner             = var.owner
-  ttl               = var.ttl
-  deployment_name   = var.deployment_name
-  vpc_cidr          = var.aws_vpc_cidr
-  private_subnets   = var.aws_private_subnets
-  public_subnets    = var.aws_public_subnets
-  cluster_name      = local.aws_eks_cluster_name
-  cluster_version   = var.aws_eks_cluster_version
-  key_pair_key_name = var.aws_key_pair_key_name
+  region                = var.aws_region
+  owner                 = var.owner
+  ttl                   = var.ttl
+  deployment_name       = var.deployment_name
+  vpc_cidr              = var.aws_vpc_cidr
+  private_subnets       = var.aws_private_subnets
+  public_subnets        = var.aws_public_subnets
+  cluster_name          = local.aws_eks_cluster_name
+  cluster_version       = var.aws_eks_cluster_version
+  key_pair_key_name     = var.aws_key_pair_key_name
+  consul_server_fqdn    = module.eks-helm.consul_server_fqdn
+  consul_serf_lan_port  = var.consul_serf_lan_port
 }
 
 module "eks-helm" {
