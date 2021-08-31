c5f4ddcd5bbf466d77dd4e69dc5befca9ba36e93
Tony Phan <tphan@hashicorp.com>
2021-08-31 18:40:52 +1000

    feat: Output public subnet ids

--- modules/infra/aws/outputs.tf
+++ modules/infra/aws/outputs.tf
@@ -3,6 +3,11 @@ output "vpc_id" {
   value       = module.vpc.vpc_id
 }
 
+output "public_subnet_ids" {
+  description = "Public subnet ids"
+  value       = module.vpc.public_subnets
+}
+
 output "private_subnet_ids" {
   description = "Private subnet ids"
   value       = module.vpc.private_subnets
