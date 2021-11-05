537699ae4f303dd5375fedbd76a136cf790b6a1a
Tony Phan <tphan@hashicorp.com>
2021-11-05 20:14:19 +1100

    chore: Rename descriptions

--- modules/consul/aws/infra/outputs.tf
+++ modules/consul/aws/infra/outputs.tf
@@ -4,12 +4,12 @@ output "cluster_id" {
 }
 
 output "ui_public_fqdn" {
-  description = "UI fqdn"
+  description = "UI public fqdn"
   value       = data.kubernetes_service.consul-ui.status.0.load_balancer.0.ingress.0.hostname
 }
 
 output "ingress_gateway_public_fqdn" {
-  description = "Ingress gateway fqdn"
+  description = "Ingress gateway public fqdn"
   value       = data.kubernetes_service.consul-ingress-gateway.status.0.load_balancer.0.ingress.0.hostname
 }
 
