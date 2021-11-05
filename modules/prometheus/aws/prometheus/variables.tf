2a5bbede8dd41c624714e0e66bde8167c250942b
Tony Phan <tphan@hashicorp.com>
2021-11-05 20:14:40 +1100

    chore: Remove deployment_name

--- modules/prometheus/aws/prometheus/variables.tf
+++ modules/prometheus/aws/prometheus/variables.tf
@@ -1,8 +1,3 @@
-variable "deployment_name" {
-  description = "Deployment name, used to prefix resources"
-  type        = string
-}
-
 variable "cluster_id" {
   description = "EKS cluster id"
   type        = string
