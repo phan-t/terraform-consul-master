50101dc07a4edab7bf97619e88f7371370f2351b
Tony Phan <tphan@hashicorp.com>
2021-11-28 22:41:53 +1100

    feat: Add payments-queue external service

--- modules/hashicups/multi-cloud/consul.tf
+++ modules/hashicups/multi-cloud/consul.tf
@@ -1,3 +1,28 @@
+resource "consul_node" "memorystore" {
+  name    = "payments-queue"
+  address = module.memorystore.host
+
+  meta = {
+    "external-node"  = "true"
+    "external-probe" = "false"
+  }
+}
+
+resource "consul_service" "memorystore" {
+  name = "payments-queue"
+  node = consul_node.memorystore.name
+  port = module.memorystore.port
+
+  check {
+    check_id = "service:redis"
+    name     = "Redis health check"
+    status   = "passing"
+    tcp      = "${module.memorystore.host}:${module.memorystore.port}"
+    interval = "30s"
+    timeout  = "3s"
+  }
+}
+
 resource "consul_config_entry" "gcp-terminating_gateway" {
   name = "gcp-terminating-gateway"
   kind = "terminating-gateway"
