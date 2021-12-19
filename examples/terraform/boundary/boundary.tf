34154e4ab5ef15e689b104c4be3f7e65ec70fc06
Tony Phan <tphan@hashicorp.com>
2021-12-19 19:27:43 +1100

    chore: Move providers to providers.tf

--- examples/terraform/boundary/boundary.tf
+++ examples/terraform/boundary/boundary.tf
@@ -1,30 +1,9 @@
-terraform {
-  required_providers {
-    boundary = {
-      source  = "hashicorp/boundary"
-      version = "1.0.5"
-    }
-  }
-}
-
 data "terraform_remote_state" "tcm" {
   backend = "local"
 
   config = {
     path = "../../../terraform.tfstate"
   }
-}
-
-provider "boundary" {
-  addr             = data.terraform_remote_state.tcm.outputs.boundary_controller_public_address
-  recovery_kms_hcl = <<EOT
-kms "awskms" {
-	purpose    = "recovery"
-	key_id     = "global_root"
-  region     = "${data.terraform_remote_state.tcm.outputs.aws_region}"
-  kms_key_id = "${data.terraform_remote_state.tcm.outputs.boundary_kms_recovery_key_id}"
-}
-EOT
 }
 
 # global scope
