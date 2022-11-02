5bc48bd946ee0641db664f5eaf5e8d840e2bca88
Tony Phan <tphan@hashicorp.com>
2022-11-03 09:40:12 +1100

    feat: Add consul security group

--- modules/infra/aws/security.tf
+++ modules/infra/aws/security.tf
@@ -20,61 +20,86 @@ module "sg-ssh" {
   ]
 }
 
-# resource "aws_security_group" "allow-ssh-public-inbound" {
-#   name_prefix = "${var.deployment_id}-allow_ssh_public_inbound-"
-#   description = "Allow ssh public inbound"
-#   vpc_id      = module.vpc.vpc_id
+module "sg-consul" {
+  source = "terraform-aws-modules/security-group/aws"
+  version     = "4.9.0"
 
-#   ingress {
-#     from_port   = 22
-#     to_port     = 22
-#     protocol    = "tcp"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
+  name        = "${var.deployment_id}-consul"
+  vpc_id      = module.vpc.vpc_id
 
-#   egress {
-#     from_port   = 0
-#     to_port     = 0
-#     protocol    = "-1"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
-# }
+  ingress_with_cidr_blocks = [
+    {
+      from_port   = 443
+      to_port     = 443
+      protocol    = "tcp"
+      description = "consul-partition-https-tcp"
+      cidr_blocks = "${module.vpc.vpc_cidr_block}"
+    },
+    {
+      from_port   = 8300
+      to_port     = 8301
+      protocol    = "tcp"
+      description = "consul-rpc-lan-serf-gosspip-tcp"
+      cidr_blocks = "${module.vpc.vpc_cidr_block}"
+    },
+    {
+      from_port   = 8300
+      to_port     = 8301
+      protocol    = "udp"
+      description = "consul-lan-serf-gosspip-udp"
+      cidr_blocks = "${module.vpc.vpc_cidr_block}"
+    },
+    {
+      from_port   = 8443
+      to_port     = 8443
+      protocol    = "tcp"
+      description = "consul-mesh-gateways"
+      cidr_blocks = "0.0.0.0/0"
+    },
+    {
+      from_port   = 8500
+      to_port     = 8502
+      protocol    = "tcp"
+      description = "consul-http-https-api-tcp"
+      cidr_blocks = "${module.vpc.vpc_cidr_block}"
+    },
+    {
+      from_port   = 8600
+      to_port     = 8600
+      protocol    = "tcp"
+      description = "consul-dns-tcp"
+      cidr_blocks = "${module.vpc.vpc_cidr_block}"
+    },
+    {
+      from_port   = 8600
+      to_port     = 8600
+      protocol    = "udp"
+      description = "consul-dns-udp"
+      cidr_blocks = "${module.vpc.vpc_cidr_block}"
+    },
+    {
+      from_port   = var.consul_serf_lan_port
+      to_port     = var.consul_serf_lan_port
+      protocol    = "tcp"
+      description = "consul-lan-serf-gosspip-tcp"
+      cidr_blocks = "${module.vpc.vpc_cidr_block}"
+    },
+    {
+      from_port   = var.consul_serf_lan_port
+      to_port     = var.consul_serf_lan_port
+      protocol    = "udp"
+      description = "consul-lan-serf-gosspip-udp"
+      cidr_blocks = "${module.vpc.vpc_cidr_block}"
+    }
+  ]
 
-# resource "aws_security_group" "allow-ssh-inbound" {
-#   name_prefix = "${var.deployment_id}-allow_ssh_inbound-"
-#   description = "Allow ssh inbound"
-#   vpc_id      = module.vpc.vpc_id
-
-#   ingress {
-#     from_port   = 22
-#     to_port     = 22
-#     protocol    = "tcp"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
-
-#   egress {
-#     from_port   = 0
-#     to_port     = 0
-#     protocol    = "-1"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
-# }
-
-# resource "aws_security_group" "allow-any-private-inbound" {
-#   name_prefix = "${var.deployment_id}-allow_any_private_inbound-"
-#   vpc_id      = module.vpc.vpc_id
-
-#   ingress {
-#     from_port   = 0
-#     to_port     = 0
-#     protocol    = "-1"
-#     cidr_blocks = module.vpc.private_subnets_cidr_blocks
-#   }
-
-#   egress {
-#     from_port   = 0
-#     to_port     = 0
-#     protocol    = "-1"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
-# }
+  egress_with_cidr_blocks = [
+    {
+      from_port   = 0
+      to_port     = 0
+      protocol    = "-1"
+      description = "any-any"
+      cidr_blocks = "0.0.0.0/0"
+    }
+  ]
+}
