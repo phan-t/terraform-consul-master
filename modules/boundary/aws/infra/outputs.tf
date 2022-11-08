6203b1fd795693298d6d97458be246da48edbf90
Tony Phan <tphan@hashicorp.com>
2022-11-08 21:51:47 +1100

    chore: Remove boundary oss

--- modules/boundary/aws/infra/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "controller_public_fqdn" {
-  description = "Public fqdn of boundary controller"
-  value       = aws_instance.controller.public_dns
-}
-
-output "kms_recovery_key_id" {
-  description = "Boundary KMS recovery key id"
-  value = aws_kms_key.recovery.id
-}
