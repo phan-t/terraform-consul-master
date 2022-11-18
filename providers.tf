2a63e63869cb143e12c5e68e2de59e4b7d5a7900
Tony Phan <tphan@hashicorp.com>
2022-11-18 21:57:32 +1100

    chore: Update google provider to latest version

--- providers.tf
+++ providers.tf
@@ -6,7 +6,7 @@ terraform {
     }
     google = {
       source  = "hashicorp/google"
-      version = "~> 4.27.0"
+      version = "~> 4.43.0"
     }
     helm = {
       source  = "hashicorp/helm"
@@ -44,7 +44,7 @@ provider "kubernetes" {
   host                   = data.aws_eks_cluster.cluster.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
   exec {
-    api_version = "client.authentication.k8s.io/v1alpha1"
+    api_version = "client.authentication.k8s.io/v1beta1"
     args        = ["eks", "get-token", "--cluster-name", data.aws_eks_cluster.cluster.name]
     command     = "aws"
   }
